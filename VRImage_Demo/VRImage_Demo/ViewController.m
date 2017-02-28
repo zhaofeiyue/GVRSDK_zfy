b957edd95cfb66faf5fe9cdfa37f53dbb78e847e
赵飞跃 <1587185188@qq.com>
2017-02-28 17:57:46 +0800

    分支 2

--- VRImage_Demo/VRImage_Demo/ViewController.m
+++ VRImage_Demo/VRImage_Demo/ViewController.m
@@ -40,6 +40,11 @@
     ///////////////
 }
 
+
+- (void)MyGitTest2{
+    ///////////////
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
